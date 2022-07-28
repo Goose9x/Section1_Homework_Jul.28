a928b68c2524ee6382377928f358133aa0d47b16
Goose9x <110024300+Goose9x@users.noreply.github.com>
2022-07-28 17:13:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// bai 1
+// let vl =prompt("Nhập điểm Vật Lý: ");
+// let hh =prompt("Nhập điểm Hóa Học: ");
+// let sh =prompt("Nhập điểm Sinh Học: ");
+// let tong= Number(vl)+Number(hh)+Number(sh);
+// let trungbinh= tong / 3;
+// alert(`Tổng điểm của thí sinh là ${tong} và điểm trung bình là ${trungbinh}`);
+
+// bai 2
+// let A = prompt("Nhập nhiệt độ C vào:");
+// let sub= A * 1.8 + 32;
+// alert(`Nhiệt độ F sau khi quy đổi là ${sub} độ F`);
+
+// bai 3
+// let A = prompt("Nhập bán kính hình tròn:");
+// let sqrt = A*A*Math.PI;
+// alert(` diện tích hình tròn là ${sqrt}`);
+
+// bai 4
+// let A = prompt("Nhập bán kính hình tròn:");
+// let sqrt = 2*A*Math.PI;
+// alert(` chu vi hình tròn là ${sqrt}`);
